8c1c64b6011ec7f36709fa2f85f5fca9b8192502
Matheus Cordeiro <matheus.cordeiro@MacBook-Pro-de-Matheus.local>
2020-03-05 00:07:25 -0300

    TapeEquilibrium solution

---
+++ Lessons/Time Complexity/TapeEquilibrium/solution.py
@@ -0,0 +1,48 @@
+#!/usr/local/bin/python3
+
+"""Task 
+
+A non-empty array A consisting of N integers is given. Array A represents numbers on a tape.
+
+Any integer P, such that 0 < P < N, splits this tape into two non-empty parts: A[0], A[1], ..., A[P − 1] and A[P], A[P + 1], ..., A[N − 1].
+
+The difference between the two parts is the value of: |(A[0] + A[1] + ... + A[P − 1]) − (A[P] + A[P + 1] + ... + A[N − 1])|
+
+In other words, it is the absolute difference between the sum of the first part and the sum of the second part.
+
+For example, consider array A such that:
+
+  A[0] = 3
+  A[1] = 1
+  A[2] = 2
+  A[3] = 4
+  A[4] = 3
+We can split this tape in four places:
+
+P = 1, difference = |3 − 10| = 7 
+P = 2, difference = |4 − 9| = 5 
+P = 3, difference = |6 − 7| = 1 
+P = 4, difference = |10 − 3| = 7 
+Write a function:
+
+def solution(A)
+
+that, given a non-empty array A of N integers, returns the minimal difference that can be achieved.
+"""
+
+def solution(A):
+    arr = []
+    size_A = len(A)
+    total = 0
+    for index, value in enumerate(A):
+        total += value
+        if index < size_A - 1:
+            arr.append(total)
+
+    minimum = 0
+    for index, left in enumerate(arr):
+        right = total - left
+        if abs(left - right) < minimum or index == 0:
+            minimum = abs(left - right)
+
+    return minimum
